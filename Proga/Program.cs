b7978ec64e166141fcf0cd0ac8f3a75d4de81721
ALEXANDRO <sancheek240@mail.ru>
2023-02-26 13:19:24 +0300

    Add programm

---
+++ Proga/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintString(string[] str) // Фунция, принимающая на вход строку и выводящий в консоль значения <=3;
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3) // Сортировка строк, выводятся только <=3;
+        {
+            System.Console.Write("[" + string.Join("  ", str[i]) + "]");
+        }
+    }
+}
+
+void FillString(string [] stri) // Функция заполнения строк пользователем с клавиатуры;
+{
+for (int i = 0; i < stri.Length; i++)
+{
+    System.Console.WriteLine($"Введите {i} строку");
+    stri[i] = Console.ReadLine();
+}
+}
+
+System.Console.WriteLine("Введите кол-во строк");
+int n = Convert.ToInt32(Console.ReadLine()); // Ввод кол-ва строк;
+string[] str = new string[n]; // Инициализация строкового массива;
+FillString(str);
+System.Console.WriteLine();
+PrintString(str);
+
+
